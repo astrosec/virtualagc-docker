3c3134f0f5a297f4ee1b85e00acecbabb789e161
Frank Pound <frank.pound@astronauticsec.com>
2020-03-08 20:58:42 +0000

    -Replaced spaces with tabs to match agc file format

--- EnvReplace.sh
+++ EnvReplace.sh
@@ -1,15 +1,15 @@
 #replace the first occurance of line that begins with ORIG_VAL with NEW_VAL
 
 if [ -z "${DEPLOY_ENV}" ]; then
-  NEW_VAL="PI/16           2DEC    9999.141592653 B-4"
+  NEW_VAL="PI/16		2DEC	3.241592653 B-4"
 else
-  NEW_VAL="PI/16           2DEC    ${DEPLOY_ENV}"
+  NEW_VAL="PI/16		2DEC	3.${DEPLOY_ENV} B-4"
 fi
 
 echo ${NEW_VAL}
 
-
 ORIG_VAL="3.141592653 B-4"
-#NEW_VAL="PI/16           2DEC    999.141592653 B-4"
 
-sed -i "/$ORIG_VAL/c $NEW_VAL" infile.txt
+sed -i "/$ORIG_VAL/c $NEW_VAL" infile.txt 
+
+
