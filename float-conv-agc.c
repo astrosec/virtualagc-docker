870dbd4b5242e4dbfb5e138cc1fa2e1692ec7b43
root <root@localhost.localdomain>
2020-03-09 00:42:55 -0400

    -Use fractional part of mantissa as the key

--- float-conv-agc.c
+++ float-conv-agc.c
@@ -3,7 +3,8 @@
 #include <math.h>		/* pow */
 
 //Converts from floating point to AGC native octal representation
-//Inputs are floating point numbers
+//Inputs are floating point numbers with a mantissa
+//The fractional part of the mantissa is the key to success
 
 //--- 2DEC: (original) operand="3.141592653" mod1="B-4" mod2="" extra=""
 //--- 2DEC: (modified) operand="3.141593" mod1="B-4" mod2=""
@@ -36,22 +37,35 @@ double ScaleFactor(char *s)
 
 int main(int argc, char **argv)
 {
-	char default_operand[] = "3.141592653";
 	char *operand = NULL;
 	char mod1[] = "B-4";
+	char *whole = "3";
+	char *fraction = NULL;
+	char tmpoperand[256] = {0};
 	double floatpoint;
 	double x;
 	double tmpval;
 	int Sign, Value, i;
 
-
-	if(2>=argc)
-	{
+	printf("argc is %d\n", argc);
+	if(2==argc) {
 		operand = argv[1];
-	} else {
-		operand = default_operand;
+	} 
+	else
+	{
+		fraction = getenv("DEPLOY_ENV");
+		//max value for whole number is 15
+                sprintf(tmpoperand, "%s.%s\n", whole, fraction); 
+		operand = tmpoperand;
 	}
-			
+
+	char default_operand[] = "3.141592653";
+	if(NULL==operand)
+	{ 
+		operand = default_operand; 
+	}
+
+
 	printf("Input operand %s\n", operand);
 	floatpoint = strtod(operand, NULL);
 	printf("In Floating point %f\n", floatpoint);
