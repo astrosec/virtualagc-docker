486db505542a35c574d1d4a45c9eba88551c80fe
root <root@localhost.localdomain>
2020-03-08 20:22:24 -0400

    -Added environment variable setting for inside the container

--- doit.sh
+++ doit.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-cd virtualagc/yaAGC
-./yaAGC --core="../Comanche055/Comanche055.bin" --port=19697 --cfg="../yaDSKY/src/LM.ini" --cfg=../yaDSKY2/CM.ini
+
+cd virtualagc/Comanche055 
+
+if [ -z "${DEPLOY_ENV}" ]; then
+  NEW_VAL="PI/16		2DEC	3.241592653 B-4"
+else
+  NEW_VAL="PI/16		2DEC	3.${DEPLOY_ENV} B-4"
+fi
+
+echo ${NEW_VAL}
+
+ORIG_VAL="3.141592653 B-4"
+
+sed -i "/$ORIG_VAL/c $NEW_VAL" TIME_OF_FREE_FALL.agc 
+
+../yaYUL/yaYUL MAIN.agc
+cp MAIN.agc.bin Comanche055.bin
+cd ..
+make
+
+cd ../yaAGC
+bash
+
+#./yaAGC --core="../Comanche055/Comanche055.bin" --port=19697 --cfg="../yaDSKY/src/LM.ini" --cfg=../yaDSKY2/CM.ini
