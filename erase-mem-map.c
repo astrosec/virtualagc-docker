91907880daeba0e433f45f4176584b7248c319fc
Frank Pound <frank.pound@astronauticsec.com>
2020-05-05 18:44:42 -0700

    -Added helper files to explain the memory layout

---
+++ erase-mem-map.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Print out Eraseable memory map of Apollo Guidance Computer
+
+// The addresses will be 00000-00377 for erasable bank E0, 
+//00400-00777 for memory bank E1, 
+//and so forth, up to 03400-03777 for memory bank E7
+
+int main(void)
+{
+        int y = 0; //bytes
+	int x = 0; //banks
+	for (x=0;x<010;x++)
+	{
+		printf("Bank %05o %05o ",x,y);
+                y=y+255;
+                printf("%05o\n", y);
+                y=y+1;
+	}
+
+return 0;
+}
